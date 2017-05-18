d061907a1823ba2d64ef6d6f1ea07b801a8f24fb
huheqing <heqing_hu@163.com>
2017-05-18 13:37:15 +0800

    tongxulu

---
+++ MyList_Node/Person.cpp
@@ -0,0 +1,17 @@
+#include "Person.h"
+ostream &operator<<(ostream &out, Person &person){
+	out << person.name << ".........." << person.phone<<endl;
+}
+
+Person &Person::operator=(Person &person){
+	this->name = person.name;
+	this->phone = person.phone;
+	return *this;
+}
+
+
+bool Person::operator==(Person &person){
+	if (this->name == person.name&&this->phone == person.phone)
+		return true;
+	return false;
+}
---
+++ MyList_Node/Person.h
@@ -0,0 +1,18 @@
+#ifndef PERSON_H
+#define PERSON_H
+#include<string>
+#include<ostream>
+using namespace std;
+
+class Person
+{
+	friend ostream &operator<<(ostream &out,Person &person);
+public:
+	string name;
+	string phone;
+	Person &operator=(Person &person);
+	bool operator==(Person &person);
+};
+
+#endif
+
